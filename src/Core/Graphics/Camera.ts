122455550d4eb067fb4bd64277dd4c9c1752ba86
MilkyStorm3 <czechu600@gmail.com>
2021-10-14 16:03:11 +0200

    CameraControls

--- src/Core/Graphics/Camera.ts
+++ src/Core/Graphics/Camera.ts
@@ -9,27 +9,76 @@ interface CameraSettings {
     far: number
 }
 
+export interface ControlKeyMap {
+
+    W: boolean,
+    S: boolean,
+    A: boolean,
+    D: boolean,
+    Space: boolean,
+    Shift: boolean
+}
 
 export class Camera {
-    private m_projectionMatrix: Matrix4x4;  
+    private m_projectionMatrix: Matrix4x4;
+    private m_settings: CameraSettings;
 
-    constructor(settings: CameraSettings, public position: Float32Vector3 = new Vector3(0,0,0)) {
-        this.m_projectionMatrix = Matrix4.perspective(settings);
+    constructor(settings: CameraSettings, public position: Float32Vector3 = new Vector3(0, 0, 0)) {
+        this.m_settings = settings;
+        this.m_projectionMatrix = Matrix4.perspective(this.m_settings);
     }
 
 
-    public m_cameraTarget = new Vector3(0,0,0); //? we assume that the camera is always looking at the origin of the coordinate system
-    public cameraDirection = new Vector3(0,0,1); //? direction
-    public cameraUp = new Vector3(0,1,0);
+    private m_cameraTarget = new Vector3(0, 0, 0); //? we assume that the camera is always looking at the origin of the coordinate system
+    private cameraUp = new Vector3(0, 1, 0);
     
+    public cameraDirection = new Vector3(0, 0, 1); //? direction
+    public cameraSpeed: number = 0.2;
+
     public GetPvMatrix(): Matrix4x4 {
-        
+
         // let mtx = Matrix4.lookAt(this.position,this.m_cameraTarget,new Vector3(0,1,0));
-      
+
         //! lookat = position + front-direction
-        let mtx = Matrix4.lookAt(this.position,this.position.add(this.cameraDirection), this.cameraUp);
+        let mtx = Matrix4.lookAt(this.position, this.position.add(this.cameraDirection), this.cameraUp);
 
         return this.m_projectionMatrix.mulByMatrix4x4(mtx);
+    }
+
+    public SetFov(fovRadian: number) {
+        this.m_settings.fovYRadian = fovRadian;
+        this.m_projectionMatrix = Matrix4x4.perspective(this.m_settings);
+    }
+
+    public OnKey(keyMap: ControlKeyMap) {
+
+        if (keyMap.W) {
+            let add = this.cameraDirection.mulByScalar(this.cameraSpeed);
+            this.position = this.position.add(add);
+        }
+
+        if (keyMap.S) {
+            let sub = this.cameraDirection.mulByScalar(this.cameraSpeed);
+            this.position = this.position.sub(sub);
+        }
+
+        if (keyMap.A) {
+            let sub2 = this.cameraDirection.cross(this.cameraUp).normalize().mulByScalar(this.cameraSpeed);
+            this.position = this.position.sub(sub2);
+        }
+        if (keyMap.D) {
+            let add2 = this.cameraDirection.cross(this.cameraUp).normalize().mulByScalar(this.cameraSpeed);
+            this.position = this.position.add(add2);
+        }
+
+        if (keyMap.Shift) {
+            this.position.y -= this.cameraSpeed;
+        }
+
+        if (keyMap.Space) {
+            this.position.y += this.cameraSpeed;
+        }
+
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import { canvas, gl, ToRadian, Init } from "./Core/Setup"
 import { VertexBuffer, IndexBuffer, LayoutAttribute } from "./Core/Graphics/Buffer"
 import { Shader } from "./Core/Graphics/Shader"
 import { RendererCommands } from "./Core/Graphics/Renderer"
-import { Camera } from "./Core/Graphics/Camera"
+import { Camera, ControlKeyMap } from "./Core/Graphics/Camera"
 import { Texture } from "./Core/Graphics/Texture"
 import { TileMap } from "./Core/Graphics/TileMap"
-import {Cube} from "./Core/Graphics/Cube"
+import { Cube } from "./Core/Graphics/Cube"
 
-import { Vector3, Vector4, Matrix4, Quaternion, Float32Vector3 } from 'matrixgl';
+import { Vector3, Vector4, Matrix4, Quaternion, Float32Vector3, Vector2 } from 'matrixgl';
 
 const log = document.querySelector("#log");
 Init();
-    
+
 let tileMap = new TileMap(1024, 416, 32);
-let tileLamp = tileMap.GetTile(7,8);
-let tile = tileMap.GetTile(2,0);
+let tileLamp = tileMap.GetTile(7, 8);
+let tile = tileMap.GetTile(2, 0);
 
 
 
@@ -33,24 +33,30 @@ texture.Load("https://i.imgur.com/afT7RAI.png");
 
 
 /*==================== MATRIX ====================== */
+let fov = 60;
 
 let camera = new Camera({
-    fovYRadian: ToRadian(60),
+    fovYRadian: ToRadian(fov),
     aspectRatio: canvas.width / canvas.height,
     near: 1,
     far: 100
 },
     new Vector3(0, 0, -6)
 );
-
+camera.cameraSpeed = 0.1;
 
 window.addEventListener('wheel', ({ deltaY }) => {
-    if (deltaY > 0) {
-        camera.position.z -= 0.3;
+
+    if (fov < 45 && fov > 1) {
+        fov += deltaY;
+        camera.SetFov(fov);
     }
-    else if (deltaY < 0) {
-        camera.position.z += 0.3;
-    }
+    // if (deltaY > 0) {
+
+    // }
+    // else if (deltaY < 0) {
+
+    // }
 });
 
 
@@ -60,36 +66,175 @@ var drag = false;
 var old_x: any, old_y: any;
 var dX = 0, dY = 0;
 
-var mouseDown = function (e: any) {
-    drag = true;
-    old_x = e.pageX, old_y = e.pageY;
-    e.preventDefault();
-    return false;
-};
+// var mouseDown = function (e: any) {
+//     drag = true;
+//     old_x = e.pageX, old_y = e.pageY;
+//     e.preventDefault();
+//     return false;
+// };
 
-var mouseUp = function (e: any) {
-    drag = false;
-};
+// var mouseUp = function (e: any) {
+//     drag = false;
+// };
 
+let X = 0, prevX = 0;
+let Y = 0, prevY = 0;
+let yaw = 180, pitch = 0;
 
 var mouseMove = function (e: any) {
-    if (!drag) return false;
-    dX = (e.pageX - old_x) * 2 * Math.PI / canvas.width,
-        dY = (e.pageY - old_y) * 2 * Math.PI / canvas.height;
-    THETA += dX;
-    PHI += dY;
-    old_x = e.pageX;
-    old_y = e.pageY;
+
+    let xOffset = X - prevX;
+    let yOffset = prevY - Y;
+    let sensitivity = 0.1;
+    let pitchLimit = 150;
+
+    yaw += xOffset * sensitivity;
+    pitch += yOffset * sensitivity;
+
+    
+
+    if (pitch > pitchLimit)
+        pitch = pitchLimit;
+
+    if (pitch < -pitchLimit)
+        pitch = -pitchLimit;
+
+    let dir = new Vector3(0, 0, 0);
+    dir.x = Math.cos(ToRadian(yaw)) * Math.cos(ToRadian(pitch));
+    dir.y = Math.sin(ToRadian(pitch));
+    dir.z = Math.sin(ToRadian(yaw)) * Math.cos(ToRadian(pitch));
+    camera.cameraDirection = dir.normalize();
 
 
+    // if (xOffset > 0) {
+    //     //left
+    //     // console.log("left"); 
+    // }
+    // if (xOffset < 0) {
+    //     //right
+
+    //     // console.log("right");
+    // }
+
+    // if (yOffset > 0) {
+    //     //up
+
+    //     // console.log("up");
+    // }
+    // if (yOffset < 0) {
+    //     //down
+
+    //     // console.log("down");
+    // }
+    // navigator.pointer.isLocked
+
+    prevX = X;
+    prevY = Y;
+
+
+    //cube rotation
+    X += e.movementX;
+    Y += e.movementY;
+
+    // if (!drag) return false;
+    // dX = (e.pageX - old_x) * 2 * Math.PI / canvas.width,
+    //     dY = (e.pageY - old_y) * 2 * Math.PI / canvas.height;
+    // // THETA += dX;
+    // PHI += dY;
+
+
+    // old_x = e.pageX;
+    // old_y = e.pageY;
 
     e.preventDefault();
 };
 
-canvas.addEventListener("mousedown", mouseDown, false);
-canvas.addEventListener("mouseup", mouseUp, false);
-canvas.addEventListener("mouseout", mouseUp, false);
-canvas.addEventListener("mousemove", mouseMove, false);
+// canvas.addEventListener("mousedown", mouseDown, false);
+// canvas.addEventListener("mouseup", mouseUp, false);
+// canvas.addEventListener("mouseout", mouseUp, false);
+// canvas.addEventListener("mousemove", mouseMove, false);
+
+// let key: string = "NULL";
+let keyMap: ControlKeyMap = {
+
+    W: false,
+    S: false,
+    A: false,
+    D: false,
+    Space: false,
+    Shift: false
+}
+
+document.addEventListener('pointerlockerror', event => {
+    // console.log("free by error");
+    // document.exitPointerLock();
+    // canvas.requestPointerLock();
+    // canvas.removeEventListener("mousemove", mouseMove);
+    // document.exitPointerLock();
+
+    // event.preventDefault();
+
+}, false);
+
+document.addEventListener('pointerlockchange', event => {
+
+    if (document.pointerLockElement !== canvas) {
+
+        console.log("free");
+
+        canvas.removeEventListener("mousemove", mouseMove);
+        document.exitPointerLock();
+
+        event.preventDefault();
+    }
+
+}, false);
+
+let SetKeyState = (key:string, val:boolean) =>{
+   switch (key) {
+        case "W": keyMap.W = val; break;
+        case "S": keyMap.S = val; break;
+        case "A": keyMap.A = val; break;
+        case "D": keyMap.D = val; break;
+        case " ": keyMap.Space = val; break;
+        case "SHIFT": keyMap.Shift = val; break;
+
+        default: break;
+    }
+}
+
+window.addEventListener("keydown", event => {
+
+    let key = event.key.toUpperCase();
+    console.log(key);
+
+    // if (key == "F5" || key == "ESCAPE") {
+    //     document.exitPointerLock();
+    // }
+    SetKeyState(key, true);
+ 
+
+    event.preventDefault();
+
+
+
+});
+
+
+window.addEventListener("keyup", ev => {
+
+    SetKeyState(ev.key.toUpperCase(), false);
+
+});
+
+canvas.requestPointerLock = canvas.requestPointerLock;
+document.exitPointerLock = document.exitPointerLock;
+
+canvas.onclick = () => {
+    canvas.addEventListener("mousemove", mouseMove, false);
+    canvas.requestPointerLock();
+}
+
 
 
 /*=================== Drawing =================== */
@@ -98,9 +243,9 @@ var THETA = 0,
     PHI = 0;
 var time_old = 0;
 
-let c1:Cube = new Cube();
-let blah:Cube = new Cube();
-let c3:Cube = new Cube();
+let c1: Cube = new Cube();
+let blah: Cube = new Cube();
+let c3: Cube = new Cube();
 
 c1.SetTexture(tile);
 blah.SetTexture(tileLamp);
@@ -113,28 +258,30 @@ var animate = function (time: number) {
 
     var dt = time - time_old;
 
-    if (!drag) {
+    /* if (!drag) {
         dX *= AMORTIZATION, dY *= AMORTIZATION;
         THETA += dX, PHI += dY;
-    }
+    } */
 
     c1.ResetTransform();
-    c1.Translate(1.2,0,2);
+    c1.Translate(1.2, 0, 2);
     c1.RotateX(PHI);
     c1.RotateY(THETA);
 
     blah.ResetTransform();
-    blah.Translate(-1,0,0);
+    blah.Translate(-1, 0, 0);
     blah.RotateX(PHI);
     blah.RotateY(THETA);
 
     c3.ResetTransform();
-    c3.Scale(0.2,0.2,0.2);
+    c3.Scale(0.2, 0.2, 0.2);
     c3.RotateX(0.7);
     c3.RotateY(0.2);
 
 
     time_old = time;
+
+    camera.OnKey(keyMap);
 
     RendererCommands.UseViewPort();
     RendererCommands.Clear();
